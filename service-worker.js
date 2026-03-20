f57bb5b125cd1346bdb8b798bfc2dbf1de83422f
gupi9163-lab <gupi9163@gmail.com>
2026-03-20 04:18:01 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // CACHE VERSION - Developer tərəfindən artırılmalıdır
-const CACHE_VERSION = 'v1.1.10';
+const CACHE_VERSION = 'v1.1.11';
 const CACHE_NAME = `bal-calc-${CACHE_VERSION}`;
 
 const ASSETS_TO_CACHE = [
